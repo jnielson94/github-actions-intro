1cca38251a170220771351c416e4c36b53b16edf
Jordan Nielson <jnielson94@gmail.com>
2021-07-01 15:50:18 -0600

    build failure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>GitHub Actions Hello World!</h1>
-      <h2>I'm Jordan Nielson (@jnielson94 on the internet usually)</h2>
-    </div>
-  );
-}
+
